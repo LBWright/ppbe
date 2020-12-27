f351d21f49b932684a8f9fc2f67763adb0af839a
Logan Wright <lwright.js@gmail.com>
2020-12-27 17:40:45 -0600

    updates to use proper port

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	go h.run()
 
 	router := gin.New()
+	router.Use(gin.Logger())
 
 	router.GET("/ws/:roomID", func(c *gin.Context) {
 		roomID := c.Param("roomID")
 		serveWs(c.Writer, c.Request, roomID)
 	})
 
-	router.Run("0.0.0.0:8082")
+	router.Run(":" + port)
 }
